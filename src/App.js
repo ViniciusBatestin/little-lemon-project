88a4a59bbbcbea31c7da7be56e7789d8c714d236
ViniciusBatestin <viniciusbatestin@gmail.com>
2024-11-10 14:40:12 +0100

    Semantic structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Main from './components/Main';
+import Nav from './components/Nav';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Home Page</h1>
-    </div>
+    <>
+        <h1>Home Page</h1>
+        <Nav/>
+        <Header/>
+        <Main/>
+        <Footer/>
+    </>
   );
 }
 
