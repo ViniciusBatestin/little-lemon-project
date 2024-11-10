068cbdafcc34604b8112c67b1774c57141647bf7
ViniciusBatestin <viniciusbatestin@gmail.com>
2024-11-10 14:39:46 +0100

    Semantic Structure

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+
+function Header() {
+  return (
+    <header>
+      <h1> My header </h1>
+      <img src="icons_assets/Logo .svg"  alt="Logo"/>
+    </header>
+  )
+}
+
+export default Header;
---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+function Nav() {
+  return(
+    <nav>
+      <h1> My nav </h1>
+      <ul>
+        <li><a href="home.html">Home</a></li>
+        <li><a href="about.html">About</a></li>
+        <li><a href="menu.html">Menu</a></li>
+        <li><a href="reservations.html">Reservations</a></li>
+        <li><a href="order-online.html">Order Online</a></li>
+        <li><a href="login.html">Login</a></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Nav;
