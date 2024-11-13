e6b510928fc808d0e500c1483b40cfbc94a710c2
ViniciusBatestin <viniciusbatestin@gmail.com>
2024-11-13 10:08:58 +0100

    arrenge footer elements and footer logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,13 @@
+import React from "react";
+import logo from "../images/logo2.png"
+
 function Footer() {
   return (
     <div className="container">
       <footer className="App-footer">
-        <h1> My footer </h1>
-        <img src="./icons_assets/Logo.svg" alt="Logo" />
-        <h2>Doormat navigation</h2>
+        <img src={logo} alt="Logo" className="App-footer-logo" />
         <ul>
+            <p><strong>Doormat <br/> navigation</strong></p>
             <li><a href="home.html">Home</a></li>
             <li><a href="about.html">About</a></li>
             <li><a href="menu.html">Menu</a></li>
@@ -14,15 +16,17 @@ function Footer() {
             <li><a href="login.html">Login</a></li>
         </ul>
 
-      <h2>Contact</h2>
-        <ul>
-          <p>Phone number: (123) 456-7890</p>
-          <p>Email: info@littlelemon.com</p>
-          <p>Addres: Las Ramblas/ Barcelona</p>
-        </ul>
+        <div>
+          <p><strong>Contact</strong></p>
+          <p>
+            <strong>Phone number:</strong> (123) 456-7890.<br /> <br/>
+            <strong>Email:</strong> info@littlelemon.com.<br /><br/>
+            <strong>Address:</strong> Las Ramblas, Barcelona.
+          </p>
+        </div>
 
-        <h2>Social Media</h2>
         <ul>
+          <p><strong>Social Media</strong></p>
           <li><a href="instagram">Instagram</a></li>
           <li><a href="Facebook.html">Facebook</a></li>
           <li><a href="tiktok.html">TikTok</a></li>
