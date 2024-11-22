96e376e3466968dc13e876e3252762a4a3cc567e
ViniciusBatestin <viniciusbatestin@gmail.com>
2024-11-22 16:09:20 +0100

    header design

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,17 +5,24 @@ function Header() {
   return (
     <div className="container">
       <header className="App-header">
-        <h1 className="markazi-text-medium">Little Lemon</h1>
-        <h2 className="karla-regular">Barcelona</h2>
-        <p className="karla-regular">we are a family owned</p>
-        <p className="karla-regular">Mediterranean restaurant,</p>
-        <p className="karla-regular">focused on traditional</p>
-        <p className="karla-regular">recipes served with a modern</p>
-        <p className="karla-regular">twist.</p>
+        <div className="header-text">
+          <h1 className="markazi-text-medium">Little Lemon</h1>
+          <h2 className="karla-regular">Marcelona</h2>
+          <p className="karla-regular">we are a family owned</p>
+          <p className="karla-regular">Mediterranean restaurant,</p>
+          <p className="karla-regular">focused on traditional</p>
+          <p className="karla-regular">recipes served with a modern</p>
+          <p className="karla-regular">twist.</p>
+        </div>
 
+    <div>
         <img src={restaurantFood} alt="restaurant food" className="header-image"/>
+    </div>
 
-        <button>Reserver a Table</button>
+    <div className="header-button">
+        <button>Reserve a Table</button>
+    </div>
+
       </header>
     </div>
   )
