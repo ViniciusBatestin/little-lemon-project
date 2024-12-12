502d3e6c7127aa7a3e928f0adac898fbe3edf30d
ViniciusBatestin <viniciusbatestin@gmail.com>
2024-12-11 21:12:45 -0300

    icons

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Main from './components/Main';
-import Nav from './components/Nav';
+import Home from './components/Home';
+import Nav from './components/Nav'
+
 
 function App() {
   return (
     <Router>
-        <Nav/>
-        <Header/>
-        <Main/>
-        <Footer/>
+      <Nav />
+      <Header />
+      <section id='menu'>
+        <Main />
+      </section>
+      <section id='about'>
+        <Footer />
+      </section>
+      <Routes>
+        < Route path='/reservation' element={<Header/>} />
+      </Routes>
     </Router>
   );
 }
